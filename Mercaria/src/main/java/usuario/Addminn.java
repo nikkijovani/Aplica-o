7ab28b5fbeb15193deb752f361aec5295b91bf92
nikkijovani <jovaniferreirainfo2017@gmail.com>
2019-09-16 18:11:00 -0300

    implementação do trabalho de luiz

---
+++ Mercaria/src/main/java/usuario/Addminn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+/**
+ *
+ * @author aluno
+ */
+public class Addminn extends Usuario{
+    
+    public String cadastrarProduto(){
+        return null;
+        
+    }
+      
+    public String boasvindas(){
+        return ("Oi adm, seja bem vindo!");
+        
+    }
+    
+    
+    
+}
